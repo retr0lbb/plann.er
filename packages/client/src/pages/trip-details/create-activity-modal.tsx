9ccb3a139230ce98f1288327229e246331fdfb40
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-07-12 13:02:10 -0300

    feat(client): add server integration with trips

--- packages/client/src/pages/trip-details/create-activity-modal.tsx
+++ packages/client/src/pages/trip-details/create-activity-modal.tsx
@@ -1,15 +1,36 @@
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps{
     closeCreateActivityModal: () => void
 }
 
 export function CreateActivityModal({closeCreateActivityModal}: CreateActivityModalProps){
+  const {tripId} = useParams()
+  
+  
+  async function createActivity(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+
+
+    const title = data.get("title")?.toString()
+    const occurs_at = data.get("occurs_at")?.toString()
+
+    await api.post(`/trips/${tripId}/activities`,  {
+      title: title,
+      occurs_at: occurs_at
+    })
+
+    window.document.location.reload()
+  }
     return(
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
                 <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-        
                   <div className="space-y-2">
                     <div className="flex items-center justify-between">
                       <h2 className="text-lg">Cadastrar atividade</h2>
@@ -21,7 +42,7 @@ export function CreateActivityModal({closeCreateActivityModal}: CreateActivityMo
                     <p className="text-sm text-zinc-400">Todos convidados podem visualizar as atividades.</p>
                   </div>
         
-                  <form className="space-y-3">
+                  <form onSubmit={createActivity} className="space-y-3">
                     <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                       <Tag className="text-zinc-400 size-5" />
                       <input
--- packages/client/src/pages/trip-details/guest-list.tsx
+++ packages/client/src/pages/trip-details/guest-list.tsx
@@ -1,41 +1,50 @@
-import { CircleDashed, UserCog } from "lucide-react"
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react"
 import { Button } from "../../components/button"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { api } from "../../lib/axios"
+
+
+interface Participants{
+    id: string
+    name: string | null,
+    email: string,
+    is_confirmed: boolean
+}
 
 
 export function GuestList(){
+    const {tripId} = useParams()
+    const [participants, setParticipants] = useState<Participants[]>([])
+
+    useEffect(()=> {
+        api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+    }, [tripId])
     return(
         <div className="space-y-6">
             <h2 className="text-xl font-semibold">Convidados</h2>
                 <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Howard Wolowitz</span>
-                        <span className="block text-xm text-zinc-400 text-sm truncate">
-                            retr0man@gmail.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Leonard Hofstader</span>
-                        <span className="block text-xm text-zinc-400 text-sm truncate">
-                            le.hof@hotmail.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Dr. Sheldon Cooper</span>
-                        <span className="block text-xm text-zinc-400 text-sm truncate">
-                            shelly_cooper.91@hotmail.com.br
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
+
+
+                    {participants.map((participant, index) => {
+                        return(
+                            <div key={participant.id} className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">{participant.name ?? `Participant ${index}`}</span>
+                                    <span className="block text-xm text-zinc-400 text-sm truncate">
+                                        {participant.email}
+                                    </span>
+                                </div>
+                                {participant.is_confirmed ? (
+                                    <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+                                ): (
+                                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                                )}
+                            </div>
+                        )
+                    })}
                 
-            </div>
+                </div>
 
             <Button size="full" variant="secondary">
                 <UserCog className="text-zinc-200 size-5" />
